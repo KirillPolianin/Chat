1ffeb3eee88f419ec6d278b178aed8adf78ff881
olyaakimova <olyaakimova9@gmail.com>
2017-05-20 13:26:07 +0300

    server side js

--- index.js
+++ index.js
@@ -3,21 +3,68 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
+//routing
+app.use(express.static(__dirname + '/public'));
+
+/*app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
-});
+});*/
 
 
+var numberOfUsers = 0;
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+    var newUser = false;
+    
+    socket.on('chat message', function(msg){
+        io.broadcast.emit('chat message', {
+            username: socket.username,
+            message: msg
+        });
+    });
+    
+    socket.on('add new user', function (username) {
+        if (newUser) return;
+
+        
+        socket.username = username;
+        ++numberOfUsers;
+        newUser = true;
+        socket.emit('login', {
+           numberOfUser: numberOfUsers 
+        });
+        
+        socket.broadcast.emit('user has joined chat', {
+            username: socket.username,
+            numberOfUsers: numberOfUsers
+        });
+    });
+    
+    socket.on('typing', function () {
+       socket.broadcast.emit('typing', {
+           username: socket.username
+       }); 
+    });
+    
+    socket.on('stop typing', fucntion () {
+        socket.broadcast.emit('stop typing', {
+              username: socket.username
+        });          
+    });
+
+    socket.on('disconnect', function () {
+        if (newUser) {
+            --numberOfUsers;
+            
+            socket.broadcast.emit('user left', {
+                username: socket.username,
+                numberOfUsers: numberOfUsers
+            });
+        }
+    });
 });
     
-
+//server listens to port
 http.listen(port, function(){
-  console.log('listening on *:' + port);
+  console.log('listening on %d:' + port);
 });
-
-/////bla bla bl
